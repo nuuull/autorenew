8017eb9bb162523d6880510dc3014f18bd72f113
nuuull <142092028+nuuull@users.noreply.github.com>
2024-06-02 02:02:43 +0800

    fixes

--- discord_webhook.sh
+++ discord_webhook.sh
@@ -7,13 +7,13 @@ function getResults() {
     -H "X-GitHub-Api-Version: 2022-11-28"
 }
 
-RESULTS=`getResults | jq '[ .jobs[0:-1][] | "\(if .conclusion == "success" then "☑️" else "❎" end) \(.name[15:-1])" ] |  join("\n")'`
+RESULTS=`getResults | jq '[ .jobs[] | select(.name | contains("refresh_token")) | "\(if .conclusion == "success" then "☑️" else "❎" end) \(.name[15:-1])" ] | join("\n")'`
 
 read -d '' REQ_BODY <<EOF
-{
-  "username": "E5 Refresh Results",
-  "content": "${RESULTS:1:-1}\\\\n\\\\nLast Run: <t:`date "+%s"`:R>"
-}
+  {
+    "username": "E5 Refresh Results",
+    "content": "${RESULTS:1:-1}\\\\n\\\\nLast Run: <t:`date "+%s"`:R>"
+  }
 EOF
 
 curl -X PATCH -H "Content-Type: application/json" -d "$REQ_BODY" $DISCORD_WEBHOOK 1> /dev/null
